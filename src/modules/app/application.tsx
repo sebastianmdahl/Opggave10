215c1944650a5c933243a56a02175884f8e8ca49
sebastianmdahl <sebastian.moen.dahl@hotmail.com>
2025-03-19 13:20:38 +0100

    MapBoxLayer

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,36 +1,33 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
+
 import { useGeographic } from "ol/proj";
+import { MapboxVectorLayer } from "ol-mapbox-style";
 
-// Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
+import Map from "ol/Map.js";
+import View from "ol/View.js";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
+
+const backgroundLayer = new MapboxVectorLayer({
+  styleUrl: "mapbox://styles/mapbox/streets-v11",
+  accessToken:
+    "pk.eyJ1Ijoic2ViYXN0aWFubWRhaGwiLCJhIjoiY204ZnZyaHNuMGN3NDJrc2p3NGs3bHFhdSJ9.j_FCm7ue8q-Wugzv4BR6xg",
+});
 
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
 const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [backgroundLayer],
+  view: new View({ center: [10.9, 59.9], zoom: 10 }),
   // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
 });
 
-// A functional React component
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
   return <div ref={mapRef}></div>;
 }
